74201f9a385788c7228e1e59975123ac14a7dad5
ayushi2212125 <166065762+ayushi2212125@users.noreply.github.com>
2025-09-22 22:39:31 +0530

    Time: 3 ms (29.62%), Space: 28.3 MB (28.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
         int count =0;
-        int maj=NULL;
+        int maj=0;
 
         for(int i=0; i<n; i++){
             if(count == 0){
@@ -14,7 +14,7 @@ public:
                 count++;
             }
             else{
-                count++;
+                count--;
             }
         }
         return maj;
