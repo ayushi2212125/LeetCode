405a8a0a8746b1d28f469f44888591ab3d2b8d30
ayushi2212125 <166065762+ayushi2212125@users.noreply.github.com>
2026-09-07 22:24:55 +0530

    Time: 337 ms (89.79%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,11 @@
+SELECT 
+case 
+    when id % 2 =1 and id<(select max(id) from Seat)
+    then id + 1
+    when id % 2 = 0
+    then id - 1
+    else id
+    end as id,
+    student
+FROM Seat
+ORDER BY id;
